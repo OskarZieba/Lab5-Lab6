2a5a90e3049e5f02070356a79fcb8c1a1b86c606
OskarZieba <oskar.zieba@karpato.pl>
2026-06-28 15:09:25 +0200

    Task3: JSON writing

--- project.py
+++ project.py
@@ -23,6 +23,22 @@ def load_data(file_path):
         print(f"Error: Unsupported input format: {ext}")
         sys.exit(1)
 
+def save_data(file_path, data):
+    _, ext = os.path.splitext(file_path)
+    ext = ext.lower()
+    
+    if ext == '.json':
+        try:
+            with open(file_path, 'w', encoding='utf-8') as f:
+                json.dump(data, f, indent=4)
+                print(f"Successfully saved data to {file_path}")
+        except Exception as e:
+            print(f"Error: Failed to write JSON to {file_path}:\n{e}")
+            sys.exit(1)
+    else:
+        print(f"Error: Unsupported output format: {ext}")
+        sys.exit(1)
+
 def main():
     if len(sys.argv) != 3:
         print("Usage: program.exe pathFile1.x pathFile2.y")
@@ -35,6 +51,7 @@ def main():
     print(f"Output file: {output_file}")
     
     data = load_data(input_file)
+    save_data(output_file, data)
 
 if __name__ == "__main__":
     main()
